fb4268ec410c2daf62618dd5339eb54360334649
JokerMisfits <lpwalker87@mail.ru>
2023-07-21 11:49:51 +0300

    1.0.0

--- views/layouts/telegram.php
+++ views/layouts/telegram.php
@@ -15,7 +15,7 @@ $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
 $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
 $this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
 $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
-$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/images/favicon.png')]);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => Yii::getAlias('@web/images/favicon.png')]);
 $name = Yii::$app->name;
 ?>
 <?php $this->beginPage() ?>
